8d6de8ac2175666b48d904943211e5e46b6d48c5
dev-pengi <siffffdoul@gmail.com>
2023-12-24 07:48:25 +0100

    feat: close the mobile menu on click outside

--- src/components/general/NavBar.tsx
+++ src/components/general/NavBar.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { FC, useState } from "react";
+import { FC, useEffect, useRef, useState } from "react";
 import Container from "./Container";
 import Image from "next/image";
 import Link from "next/link";
@@ -13,6 +13,23 @@ const NavBar: FC = () => {
   const handleNavToggle = () => {
     setIsNavOpened((prev) => !prev);
   };
+
+  const wrapperRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    function handleClickOutside(event: any) {
+      if (wrapperRef.current && !wrapperRef.current.contains(event.target)) {
+        setIsNavOpened(false);
+      }
+    }
+
+    document.addEventListener("mousedown", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
   return (
     <nav className="h-[80px] z-9 relative bg-transparent">
       <Container className="w-full h-full flex items-center">
@@ -42,34 +59,37 @@ const NavBar: FC = () => {
         <div className="flex-1 justify-end md:flex hidden">
           <Button size="sm">Get Started</Button>
         </div>
-        <div
-          className={`md:hidden block text-[32px] ${
-            isNavOpened ? "text-primary" : "text-dark"
-          }`}
-          onClick={handleNavToggle}
-        >
-          <MenuIcon />
-        </div>
+        <div>
+          <div
+            ref={wrapperRef}
+            className={`md:hidden block text-[32px] ${
+              isNavOpened ? "text-primary" : "text-dark"
+            }`}
+            onClick={handleNavToggle}
+          >
+            <MenuIcon />
+          </div>
 
-        <div
-          className={`fixed w-full left-0 h-max py-6 px-3 bg-dark z-10 rounded-t-xl duration-200 ${
-            isNavOpened ? "-bottom-0.5" : "-bottom-full"
-          }`}
-        >
-          <ul className="gap-5 flex flex-col justify-center items-center text-center text-white">
-            {navLinks.map((link, index) => (
-              <li>
-                <Link
-                  className="capitalize duration-200 hover:text-primary"
-                  key={`nav-link-${link.name}-${index}`}
-                  href={link.route}
-                >
-                  {link.name}
-                </Link>
-              </li>
-            ))}
-            <Button className="">Get Started</Button>
-          </ul>
+          <div
+            className={`fixed w-full left-0 h-max py-6 px-3 bg-dark z-10 rounded-t-xl duration-200 ${
+              isNavOpened ? "-bottom-0.5" : "-bottom-full"
+            }`}
+          >
+            <ul className="gap-5 flex flex-col justify-center items-center text-center text-white">
+              {navLinks.map((link, index) => (
+                <li>
+                  <Link
+                    className="capitalize duration-200 hover:text-primary"
+                    key={`nav-link-${link.name}-${index}`}
+                    href={link.route}
+                  >
+                    {link.name}
+                  </Link>
+                </li>
+              ))}
+              <Button className="">Get Started</Button>
+            </ul>
+          </div>
         </div>
       </Container>
     </nav>
